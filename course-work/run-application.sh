65d585ef6559748b5cff4a89c5dd295a28d599a5
yevheniiStepaniuk <yevhenii.stepaniuk@gmail.com>
2019-06-13 09:41:39 -0200

    run application

---
+++ course-work/run-application.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker-compose -f ./docker-compose.spark.yml up --build course-app
--- course-work/run-spark.sh
+++ course-work/run-spark.sh
@@ -5,5 +5,3 @@ docker-compose -f ./docker-compose.spark.yml up -d spark-master
 docker-compose -f ./docker-compose.spark.yml up -d spark-worker
 
 docker-compose -f ./docker-compose.spark.yml scale spark-worker=3
-
-docker-compose -f ./docker-compose.spark.yml up --build course-app
